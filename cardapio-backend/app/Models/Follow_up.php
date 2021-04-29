c87b1ae6a8faad7eec658d2174f0a95939e9a2d7
thyerre <thyerre321@gmail.com>
2021-04-28 21:56:18 -0300

    correçao model follow

--- cardapio-backend/app/Models/Follow_up.php
+++ cardapio-backend/app/Models/Follow_up.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Follow_up extends Model
 {
     use HasFactory;
+    
+    protected $table = 'follow_up';
+    protected $primaryKey = 'id';
+    protected $fillable = ['id', 'description', 'is_active', 'is_free','value', 'min', 'max', 'id_product'];
 }
