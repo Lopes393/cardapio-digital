c06ee627598e18fde8cbc3c58c760176eaf405ce
thyerre <thyerre321@gmail.com>
2021-05-01 21:29:42 -0300

    time para aparecer a imagem da mulher

--- cardapio-frontend/src/components/OrderClear/index.tsx
+++ cardapio-frontend/src/components/OrderClear/index.tsx
@@ -2,9 +2,12 @@ import Lottie from 'react-lottie';
 import { Container, BoxHamb } from './styles';
 import animationData from './../../assets/lotties/duvida.json';
 import animationDataDelivery from './../../assets/lotties/delivery.json';
+import { useState } from 'react';
 
 
 export function OrderClear() {
+    const [printInTela, setPrintInTela] = useState<boolean>(false);
+
 
     const defaultOptions = {
         loop: true,
@@ -23,24 +26,32 @@ export function OrderClear() {
         }
     };
 
+    setTimeout(() => {
+        setPrintInTela(true)
+    },200);
+
     return (
         <Container>
-            <BoxHamb>
-                <Lottie 
-                    options={defaultOptions}
-                    height={90}
-                    width={90}
-                />
-            </BoxHamb>
-            <strong>Você ainda não selecionou um produto.</strong>
-            <p>Bora fazer um pedido de algo delicioso</p>
-            <BoxHamb>
-                <Lottie 
-                        options={defaultOptionsDelivery}
+            { printInTela ? 
+            <>
+                <BoxHamb>
+                    <Lottie 
+                        options={defaultOptions}
                         height={90}
                         width={90}
                     />
-            </BoxHamb>
+                </BoxHamb>
+                <strong>Você ainda não selecionou um produto.</strong>
+                <p>Bora fazer um pedido de algo delicioso</p>
+                <BoxHamb>
+                    <Lottie 
+                            options={defaultOptionsDelivery}
+                            height={90}
+                            width={90}
+                        />
+                </BoxHamb>
+            </>
+            : '' }
         </Container>              
     )
 }
