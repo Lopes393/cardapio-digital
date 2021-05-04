22125ed1bf0bcc606eb495703953e5ec32d4b513
thyerre <thyerre321@gmail.com>
2021-05-04 13:30:02 -0300

    adicionando atualizaçao automatica de cache

--- cardapio-frontend/src/App.tsx
+++ cardapio-frontend/src/App.tsx
@@ -3,14 +3,31 @@ import { Header } from "./components/Header";
 import { Menu } from "./components/Menu";
 import { Splash } from "./components/Splash";
 import { GlobalStyle } from "./styles/global";
+import { version } from "./../package.json";
 
 
 export function App() {
   const [loader, setLoader] = useState<boolean>(true);
 
   useEffect(() => {
+    verifyVersion();
     setTimeout(() => { setLoader(false) }, 3000);
   }, [])
+
+  function verifyVersion() {
+		if (version != getVersionLocalStorage()) {
+			setVersionLocalStorage(version);
+			window.location.reload();
+		}
+	}
+
+	function getVersionLocalStorage() {
+		return localStorage.getItem("wiseller_version");
+	}
+
+	function setVersionLocalStorage(version: any) {
+		return localStorage.setItem("wiseller_version", version);
+	}
 
   return (
     <div className="App">
