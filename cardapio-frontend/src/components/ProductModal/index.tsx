7d6f03339bcabc2b76f89d50d89e527086f6477a
Murilo Lopes <mloopes11@gmail.com>
2021-05-04 12:49:45 -0300

    margin icons additionals

--- cardapio-frontend/src/components/ProductModal/index.tsx
+++ cardapio-frontend/src/components/ProductModal/index.tsx
@@ -1,130 +1,166 @@
-import { useEffect, useState } from 'react';
-import {BtnClose, Container, ContentHeader, Content, ContentAction, ContentTotal, Title} from './styles';
-import cancel from './../../assets/img/icons/cancel.svg';
-import Modal from 'react-modal';
-import { api } from '../../service/api';
-import { ImagemProduct } from '../../service/ImagemProduct';
-import imgMais from './../../assets/img/icons/mais.svg';
-import imgMenos from './../../assets/img/icons/menos.svg';
-import { Storage } from '../../service/Storage';
+import { useEffect, useState } from "react";
+import {
+  BtnClose,
+  Container,
+  ContentHeader,
+  Content,
+  ContentAction,
+  ContentTotal,
+  Title,
+  Additionals,
+} from "./styles";
+import cancel from "./../../assets/img/icons/cancel.svg";
+import Modal from "react-modal";
+import { api } from "../../service/api";
+import { ImagemProduct } from "../../service/ImagemProduct";
+import imgMais from "./../../assets/img/icons/mais.svg";
+import imgMenos from "./../../assets/img/icons/menos.svg";
+import { Storage } from "../../service/Storage";
 
-export function ProductModal({status , product, setOpenModalProduct, setTelaGeral}: any) {
-    const [additionals, setAdditionals] = useState<any[]>([]);
-    const [total, setTotal] = useState<number>(0);
+export function ProductModal({
+  status,
+  product,
+  setOpenModalProduct,
+  setTelaGeral,
+}: any) {
+  const [additionals, setAdditionals] = useState<any[]>([]);
+  const [total, setTotal] = useState<number>(0);
 
-    useEffect(() => {
-        getProducts();
-    }, [status]) 
-    
-    function getProducts() {
-        setAdditionals([])
-        api.get(`additional/${product.id}`).then((response) => {setAdditionals(response.data);calcularTotal();})
-    }
-    
-    function setOpenModalProductAction(status: boolean) {
-        setOpenModalProduct(status)
-    }
+  useEffect(() => {
+    getProducts();
+  }, [status]);
 
-    function alterTelaToOrder() {
-        setTelaGeral('order')
-    }
+  function getProducts() {
+    setAdditionals([]);
+    api.get(`additional/${product.id}`).then((response) => {
+      setAdditionals(response.data);
+      calcularTotal();
+    });
+  }
 
-    function getAdditional() {
-        let selecteds = [];
-        for(let item of additionals) {
-            if (item.qt_item > 0) {
-                selecteds.push(item);
-            }
-        }
+  function setOpenModalProductAction(status: boolean) {
+    setOpenModalProduct(status);
+  }
 
-        return selecteds;
-    }
+  function alterTelaToOrder() {
+    setTelaGeral("order");
+  }
 
-    function selectProduct() {
-        let order  = Storage('order');
-        let productSave = product;
-        
-        productSave.item = getAdditional();
-
-        if (order) {
-            order = [productSave, ...order] 
-        } else {
-            order = [productSave] 
-        }
-
-        Storage('order', order);
-        alterTelaToOrder() 
+  function getAdditional() {
+    let selecteds = [];
+    for (let item of additionals) {
+      if (item.qt_item > 0) {
+        selecteds.push(item);
+      }
     }
 
-    function alterQuantity(item: any, incre: boolean = true) {
-        if (incre) {
-            item.qt_item += 1;
-        } else {
-            if (item.qt_item >= 1) {
-                item.qt_item -= 1;
-            }
-        }
+    return selecteds;
+  }
 
-        const itensCopy = Array.from(additionals);
-        itensCopy[additionals.indexOf(item)] = item;
-        setAdditionals(itensCopy);
+  function selectProduct() {
+    let order = Storage("order");
+    let productSave = product;
 
-        calcularTotal();
+    productSave.item = getAdditional();
+
+    if (order) {
+      order = [productSave, ...order];
+    } else {
+      order = [productSave];
     }
 
-    function calcularTotal() {
-        let total = parseFloat(product.value);
+    Storage("order", order);
+    alterTelaToOrder();
+  }
 
-        for(let item of additionals) {
-            if (item.qt_item > 0) {
-                total += (item.qt_item * item.value); 
-            }
-        }
-
-        setTotal(total)
+  function alterQuantity(item: any, incre: boolean = true) {
+    if (incre) {
+      item.qt_item += 1;
+    } else {
+      if (item.qt_item >= 1) {
+        item.qt_item -= 1;
+      }
     }
 
-    return (
-        <Container>
-             <Modal 
-                isOpen={status}  
-                onRequestClose={() => setOpenModalProductAction(false)}
-                overlayClassName="react-modal-overlay"
-                ariaHideApp={false}
-                className="react-modal-content"
-            >
-            <BtnClose>
-                <img src={cancel} alt="" onClick={()=> setOpenModalProductAction(false)}/>
-            </BtnClose>
-            <ContentHeader>
-                <strong>{product?.name}</strong>
-                <p>{product?.description}</p>
-            </ContentHeader>
-            <Content>
-                {additionals.length > 0 ? <Title>Acrecimos</Title>: ''}
-                {additionals.map((item) => (
-                    <div key={item.id}>
-                        <div>
-                            <img src={ImagemProduct(item.icon)} alt=""/>
-                            <strong>{item?.name}</strong>
-                            <p>{parseFloat(item?.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
-                            <img src={imgMenos} onClick={()=> alterQuantity(item, false)} />
-                            <p>{item?.qt_item}</p>
-                            <img src={imgMais} onClick={()=> alterQuantity(item)} />
-                        </div>
-                    </div>
-                ))}
-            </Content>
-            <ContentTotal>
-                <strong>Total: {total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
+    const itensCopy = Array.from(additionals);
+    itensCopy[additionals.indexOf(item)] = item;
+    setAdditionals(itensCopy);
 
-            </ContentTotal>
+    calcularTotal();
+  }
 
-            <ContentAction>
-                    <button onClick={()=> setOpenModalProductAction(false)}>Cancelar</button>
-                    <button onClick={()=> selectProduct()}>Selecionar</button>
-            </ContentAction>
-            </Modal>
-        </Container>
-    )
+  function calcularTotal() {
+    let total = parseFloat(product.value);
+
+    for (let item of additionals) {
+      if (item.qt_item > 0) {
+        total += item.qt_item * item.value;
+      }
+    }
+
+    setTotal(total);
+  }
+
+  return (
+    <Container>
+      <Modal
+        isOpen={status}
+        onRequestClose={() => setOpenModalProductAction(false)}
+        overlayClassName="react-modal-overlay"
+        ariaHideApp={false}
+        className="react-modal-content"
+      >
+        <BtnClose>
+          <img
+            src={cancel}
+            alt=""
+            onClick={() => setOpenModalProductAction(false)}
+          />
+        </BtnClose>
+        <ContentHeader>
+          <strong>{product?.name}</strong>
+          <p>{product?.description}</p>
+        </ContentHeader>
+        <Content>
+          {additionals.length > 0 ? <Title>Acrecimos</Title> : ""}
+          {additionals.map((item) => (
+            <Additionals key={item.id}>
+              <img src={ImagemProduct(item.icon)} alt="" />
+              <strong>{item?.name}</strong>
+              <p className="value">
+                {parseFloat(item?.value).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
+              <div>
+                <img
+                  src={imgMenos}
+                  onClick={() => alterQuantity(item, false)}
+                />
+                <p>{item?.qt_item}</p>
+                <img src={imgMais} onClick={() => alterQuantity(item)} />
+              </div>
+            </Additionals>
+          ))}
+        </Content>
+        <ContentTotal>
+          <strong>
+            Total:{" "}
+            {total.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </strong>
+        </ContentTotal>
+
+        <ContentAction>
+          <button onClick={() => setOpenModalProductAction(false)}>
+            Cancelar
+          </button>
+          <button onClick={() => selectProduct()}>Selecionar</button>
+        </ContentAction>
+      </Modal>
+    </Container>
+  );
 }
--- cardapio-frontend/src/components/ProductModal/styles.ts
+++ cardapio-frontend/src/components/ProductModal/styles.ts
@@ -1,125 +1,147 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display:flex;
-    flex-direction: column;
-    width: 100vw;
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
 `;
 
 export const BtnClose = styled.div`
-    display:flex;
-    flex-direction: column;
-    width: 90%;
-    margin-left: 5%;
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  margin-left: 5%;
 
-    img {
-        width: 20px;
-        position: absolute;
-        top: 0;
-        right: 0;
-        margin: 10px 10px;
-    }
+  img {
+    width: 20px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 10px 10px;
+  }
 `;
 
 export const ContentHeader = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
 
-    strong {
-        font-size: 1.2rem;
-    }
+  strong {
+    font-size: 1.2rem;
+  }
 
-    p {
-        font-size: .7rem;
-    }
+  p {
+    font-size: 0.7rem;
+  }
 `;
 
 export const Title = styled.strong`
-    margin: 7%;
+  margin: 7%;
 `;
 
 export const Content = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
 
+  div {
+    padding: 0.5rem;
     div {
-        padding: .5rem;
-        div {
-            display:flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
 
-            strong , p {
-                margin-right: 20px;
-            }
+      strong,
+      p {
+        margin-right: 20px;
+      }
 
-            img {
-                width: 20px;
-                height: 20px;
-                margin-right: 10%;
+      img {
+        width: 20px;
+        height: 20px;
+        margin-right: 10%;
 
-                &:active {
-                    width: 25px;
-                    height: 25px;
-                }
-
-                transition: filter 0.2s;
-
-                &:hover {
-                    filter: brightness(0.85);
-                }
-            }
+        &:active {
+          width: 25px;
+          height: 25px;
         }
-    }
-
-`;
-
-export const ContentAction = styled.div`
-    display:flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    width: 90%;
-    margin-top: 10%;
-    
-    button:first-child {
-        margin-right: 5%;
-        margin-left: 5%;
-        background-color: #fff;
-    }
-
-    button:last-child {
-        background-color: #28a265;
-        color: #fff;
-    }
-
-    button {
-        padding: 10px 30px;
-        box-shadow: none;
-        border-radius: 5px;
-        border: none;
 
         transition: filter 0.2s;
 
         &:hover {
-            filter: brightness(0.85);
+          filter: brightness(0.85);
         }
+      }
     }
+  }
+`;
+
+export const ContentAction = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  margin-top: 10%;
+
+  button:first-child {
+    margin-right: 5%;
+    margin-left: 5%;
+    background-color: #fff;
+  }
+
+  button:last-child {
+    background-color: #28a265;
+    color: #fff;
+  }
+
+  button {
+    padding: 10px 30px;
+    box-shadow: none;
+    border-radius: 5px;
+    border: none;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.85);
+    }
+  }
 `;
 
 export const ContentTotal = styled.div`
-    display:flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    width: 90%;
-    margin-top: 10%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  margin-top: 10%;
 `;
 
+export const Additionals = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 1rem;
+    margin: 0;
+  }
+  strong {
+    width: 8rem;
+    margin-left: 0.5rem;
+  }
+  p.value {
+    width: 3rem;
+    margin: 0 !important;
+  }
+  div {
+    width: 5rem;
+  }
+`;
