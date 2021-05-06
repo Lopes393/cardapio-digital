33bfb39d997b62b751182a10e0d2fca061ccf43e
Murilo Lopes <mloopes11@gmail.com>
2021-05-06 20:22:35 -0300

    Cupom de desconto

--- cardapio-frontend/src/components/OrderModal/index.tsx
+++ cardapio-frontend/src/components/OrderModal/index.tsx
@@ -1,166 +1,264 @@
-import { useEffect, useState } from 'react';
-import {BtnClose, Container, ContentHeader, Content, ContentAction, ContentTotal, Title} from './styles';
-import cancel from './../../assets/img/icons/cancel.svg';
-import Modal from 'react-modal';
-import { Storage } from '../../service/Storage';
-import { Message } from '../../service/Message';
+import { useEffect, useState } from "react";
+import {
+  BtnClose,
+  Container,
+  ContentHeader,
+  Content,
+  ContentAction,
+  ContentTotal,
+  Title,
+} from "./styles";
+import cancel from "./../../assets/img/icons/cancel.svg";
+import Modal from "react-modal";
+import { Storage } from "../../service/Storage";
+import { Message } from "../../service/Message";
 
-export function OrderModal({status , product, setOpenModalOrder, setTelaGeral}: any) {
-    const [totalProducts, setTotalProducts] = useState<number>(0);
-    const [totalDelivery, setTotalDelivery] = useState<number>(0);
+export function OrderModal({
+  status,
+  product,
+  setOpenModalOrder,
+  setTelaGeral,
+}: any) {
+  const [totalProducts, setTotalProducts] = useState<number>(0);
+  const [totalDelivery, setTotalDelivery] = useState<number>(0);
 
-    const [isCard, setIsCard] = useState<string>('money');
-    const [isDelivery, setIsDelivery] = useState<string>('');
+  const [isCard, setIsCard] = useState<string>("money");
+  const [isDelivery, setIsDelivery] = useState<string>("");
 
-    const [thing, setThing] = useState<string>('');
-    const [name, setName] = useState<string>('');
-    const [address, setAddress] = useState<string>('');
+  const [thing, setThing] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [cupom, setCupom] = useState<string>("");
 
-    useEffect(() => {
-        calcularTotal();
-        getInfoLocal();
-    }, [status]) 
+  useEffect(() => {
+    calcularTotal();
+    getInfoLocal();
+  }, [status]);
 
-    function getInfoLocal() {
-        let adress = Storage('address');
-        let thing = Storage('thing');
-        let name = Storage('name');
+  function getInfoLocal() {
+    let adress = Storage("address");
+    let thing = Storage("thing");
+    let name = Storage("name");
+    let cupom = Storage("cupom");
 
-        if (adress) {
-            setAddress(adress);
+    if (adress) {
+      setAddress(adress);
+    }
+
+    if (thing) {
+      setThing(thing);
+    }
+
+    if (name) {
+      setName(name);
+    }
+    if (cupom) {
+      setCupom(cupom);
+    }
+  }
+
+  function setOpenModalProductAction(status: boolean) {
+    setOpenModalOrder(status);
+  }
+
+  function sendOrder() {
+    const config: any = {
+      address,
+      thing,
+      isCard,
+      isDelivery,
+      totalDelivery,
+      totalProducts,
+      name,
+      cupom,
+    };
+    Storage("config", config, true);
+    Storage("address", address, true);
+    Storage("thing", thing, true);
+    Storage("name", name, true);
+    Storage("cupom", cupom, true);
+
+    Message(config, Storage("order"));
+  }
+
+  function calcularTotal() {
+    let order = Storage("order");
+    let totalDelivery = parseFloat(Storage("delivery"));
+    let totalProduct = 0;
+    if (order) {
+      for (let productInOrder of order) {
+        totalProduct += parseFloat(productInOrder.value);
+
+        for (let additional of productInOrder.item) {
+          totalProduct += parseFloat(additional.value);
         }
+      }
 
-        if (thing) {
-            setThing(thing);
-        }
-
-        if (name) {
-            setName(name);
-        }
+      setTotalProducts(totalProduct);
+      setTotalDelivery(totalDelivery);
     }
-    
-    
-    function setOpenModalProductAction(status: boolean) {
-        setOpenModalOrder(status)
-    }
+  }
 
-    function sendOrder() {
-        const config: any = {
-            address,
-            thing,
-            isCard,
-            isDelivery,
-            totalDelivery,
-            totalProducts,
-            name
-        }
-        Storage('config', config, true);
-        Storage('address', address, true);
-        Storage('thing', thing, true);
-        Storage('name', name, true);
+  function nameChange(event: any) {
+    setName(event.target.value);
+  }
+  function cupomChange(event: any) {
+    setCupom(event.target.value);
+  }
 
-        Message(config, Storage('order'))
+  function thingChange(event: any) {
+    setThing(event.target.value);
+  }
 
-        
-    }
+  function addressChange(event: any) {
+    setAddress(event.target.value);
+  }
 
-    function calcularTotal() {
-        let order = Storage('order');
-        let totalDelivery = parseFloat(Storage('delivery'));
-        let totalProduct = 0;
-        if (order) {
-            
-            for(let productInOrder of order) {
-                totalProduct += parseFloat(productInOrder.value);
-    
-                for(let additional of productInOrder.item) {
-                    totalProduct += parseFloat(additional.value);
-                }
-    
-            }
-    
-            setTotalProducts(totalProduct)
-            setTotalDelivery(totalDelivery)
-        }
-    }
+  return (
+    <Container>
+      <Modal
+        isOpen={status}
+        onRequestClose={() => setOpenModalProductAction(false)}
+        overlayClassName="react-modal-overlay"
+        ariaHideApp={false}
+        className="react-modal-content"
+      >
+        <BtnClose>
+          <img
+            src={cancel}
+            alt=""
+            onClick={() => setOpenModalProductAction(false)}
+          />
+        </BtnClose>
 
-    function nameChange(event: any) {    
-        setName(event.target.value); 
-    }
+        <ContentHeader>
+          <strong>Informações de entrega e pagamento</strong>
+        </ContentHeader>
 
-    function thingChange(event: any) {    
-        setThing(event.target.value); 
-    }
+        <Content>
+          <input
+            type="text"
+            placeholder="Qual seu nome?"
+            value={name}
+            onChange={nameChange}
+          />
+          <div>
+            {isCard === "card" ? (
+              <button className="active" onClick={() => setIsCard("card")}>
+                Cartão
+              </button>
+            ) : (
+              <button onClick={() => setIsCard("card")}>Cartão</button>
+            )}
+            {isCard === "money" ? (
+              <button className="active" onClick={() => setIsCard("money")}>
+                Dinheiro
+              </button>
+            ) : (
+              <button onClick={() => setIsCard("money")}>Dinheiro</button>
+            )}
+          </div>
 
-    function addressChange(event: any) {    
-        setAddress(event.target.value); 
-    }
+          {isCard === "money" ? (
+            <>
+              <input
+                type="text"
+                placeholder="precisa de troco?"
+                value={thing}
+                onChange={thingChange}
+              />
+              <p>Ex: troco pra 50 ou não</p>
+            </>
+          ) : (
+            ""
+          )}
 
+          {thing !== "" || isCard == "card" ? (
+            <div>
+              {isDelivery === "delivery" ? (
+                <button
+                  className="active"
+                  onClick={() => setIsDelivery("delivery")}
+                >
+                  Entregar
+                </button>
+              ) : (
+                <button onClick={() => setIsDelivery("delivery")}>
+                  Entregar
+                </button>
+              )}
+              {isDelivery === "goway" ? (
+                <button
+                  className="active"
+                  onClick={() => setIsDelivery("goway")}
+                >
+                  Vou buscar
+                </button>
+              ) : (
+                <button onClick={() => setIsDelivery("goway")}>
+                  Vou buscar
+                </button>
+              )}
+            </div>
+          ) : (
+            ""
+          )}
 
-    return (
-        <Container>
-             <Modal 
-                isOpen={status}  
-                onRequestClose={() => setOpenModalProductAction(false)}
-                overlayClassName="react-modal-overlay"
-                ariaHideApp={false}
-                className="react-modal-content"
-            >
+          {isDelivery === "delivery" ? (
+            <>
+              <input
+                type="text"
+                placeholder="Qual o seu endereço?"
+                value={address}
+                onChange={addressChange}
+              />
+              <p>Ex: Rua M19 setor Central n° 14</p>
+            </>
+          ) : (
+            ""
+          )}
+          <input
+            type="text"
+            placeholder="CUPOM"
+            value={cupom}
+            onChange={cupomChange}
+          />
+        </Content>
 
-            <BtnClose>
-                <img src={cancel} alt="" onClick={()=> setOpenModalProductAction(false)}/>
-            </BtnClose>
+        <ContentTotal>
+          <strong>
+            Produtos:{" "}
+            {totalProducts.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </strong>
+          {isDelivery === "delivery" ? (
+            <strong>
+              Frete:{" "}
+              {totalDelivery.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </strong>
+          ) : (
+            ""
+          )}
+          <strong>
+            Total:{" "}
+            {(
+              totalProducts + (isDelivery === "delivery" ? totalDelivery : 0)
+            ).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+          </strong>
+        </ContentTotal>
 
-            <ContentHeader>
-                <strong>Informações de entrega e pagamento</strong>
-            </ContentHeader>
-
-            <Content>
-
-                <input type="text" placeholder="Qual seu nome?" value={name} onChange={nameChange}/>
-                <div>
-                    {isCard === 'card' ? <button className="active" onClick={()=> setIsCard('card')}>Cartão</button> : <button onClick={()=> setIsCard('card')}>Cartão</button>}
-                    {isCard === 'money' ? <button className="active" onClick={()=> setIsCard('money')}>Dinheiro</button> : <button onClick={()=> setIsCard('money')}>Dinheiro</button>}
-                </div>
-
-
-                {isCard === 'money' ? 
-                    <>
-                        <input type="text" placeholder="precisa de troco?" value={thing} onChange={thingChange}/>
-                        <p>Ex: troco pra 50 ou não</p>
-                    </>
-                : '' }
-
-                {thing !== '' || isCard == 'card'? 
-                <div>
-                    {isDelivery === 'delivery' ? <button className="active"onClick={()=> setIsDelivery('delivery')}>Entregar</button> : <button onClick={()=> setIsDelivery('delivery')}>Entregar</button>}
-                    {isDelivery === 'goway' ? <button className="active" onClick={()=> setIsDelivery('goway')}>Vou buscar</button> : <button onClick={()=> setIsDelivery('goway')}>Vou buscar</button>}
-                </div>
-                : '' }
-                
-                {isDelivery === 'delivery' ? 
-                    <>
-                        <input type="text" placeholder="Qual o seu endereço?" value={address} onChange={addressChange}/>
-                        <p>Ex: Rua M19 setor Central n° 14</p>
-                    </>
-                : '' }
-
-            </Content>
-
-
-            <ContentTotal>
-                <strong>Produtos: {totalProducts.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
-                {isDelivery === 'delivery' ? 
-                    <strong>Frete: {totalDelivery.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
-                : ''}
-                <strong>Total: {(totalProducts + ( isDelivery === 'delivery' ? totalDelivery : 0)).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
-            </ContentTotal>
-
-            <ContentAction>
-                    <button onClick={()=> setOpenModalProductAction(false)}>Voltar</button>
-                    <button onClick={()=> sendOrder()}>Enviar</button>
-            </ContentAction>
-            </Modal>
-        </Container>
-    )
+        <ContentAction>
+          <button onClick={() => setOpenModalProductAction(false)}>
+            Voltar
+          </button>
+          <button onClick={() => sendOrder()}>Enviar</button>
+        </ContentAction>
+      </Modal>
+    </Container>
+  );
 }
--- cardapio-frontend/src/service/Message.ts
+++ cardapio-frontend/src/service/Message.ts
@@ -1,55 +1,70 @@
 import { Storage } from "./Storage";
 
 export function Message(config: any, order: any) {
-    let text = `Pedido de ${config.name}: \n`;
+  let text = `Pedido de ${config.name}: \n`;
 
-    for(let product of order) {
-        text += `1 ${product.name} de ${formatMoeda(product.value)}\n`;
+  for (let product of order) {
+    text += `1 ${product.name} de ${formatMoeda(product.value)}\n`;
 
-        if (product.item !== undefined && product.item.length > 0) {
-            text += `com adicionais: `;
-            for(let additional of product.item) {
-                text += ` ${additional.qt_item}x ${additional.name} de ${formatMoeda(additional.value)}, `;
-            }
-            text = text.replace(/,\s*$/, "")
-        }
-
-        text += '\n';
+    if (product.item !== undefined && product.item.length > 0) {
+      text += `com adicionais: `;
+      for (let additional of product.item) {
+        text += ` ${additional.qt_item}x ${additional.name} de ${formatMoeda(
+          additional.value
+        )}, `;
+      }
+      text = text.replace(/,\s*$/, "");
     }
 
-    
-    if (config.isDelivery === 'delivery') {
-        text += `\nPara entrega, endereço: ${config.address}`;
+    text += "\n";
+  }
+
+  if (config.isDelivery === "delivery") {
+    text += `\nPara entrega, endereço: ${config.address}`;
+  } else {
+    text += "\nVou retirar no local ";
+  }
+
+  if (config.isCard === "card") {
+    text += `\nO pagamento é no cartão`;
+  } else {
+    text += `\nO pagamento é no dinheiro `;
+
+    if (config.thing) {
+      text += `\nTroco: ${config.thing}\n`;
     } else {
-        text += '\nVou retirar no local ';
+      text += `sem troco\n`;
+    }
+  }
+
+  if (config.cupom) {
+    text += `\nCupom: ${config.cupom}\n`;
+  }
+
+  let phone = Storage("phone");
+
+  if (!phone) {
+    if (
+      window.confirm(
+        "Precisamos atualizar o cardapio! \n Você não perderá seu pedido"
+      )
+    ) {
+      window.location.href = `https://api.whatsapp.com/send?phone=5564999187173&text=${window.encodeURIComponent(
+        text
+      )}`;
     }
 
-    if (config.isCard === 'card') {
-        text += `\nO pagamento é no cartão`;
-    } else {
-        text += `\nO pagamento é no dinheiro `;
+    return;
+  }
 
-        if (config.thing) {
-            text += `\nTroco: ${config.thing}\n`;
-        } else {
-            text += `sem troco\n`;
-        }
-    }
-
-    
-    let phone = Storage('phone');
-
-    if (!phone) {
-        if(window.confirm('Precisamos atualizar o cardapio! \n Você não perderá seu pedido')) {
-            window.location.href = `https://api.whatsapp.com/send?phone=5564999187173&text=${window.encodeURIComponent(text)}`;
-        } 
-
-        return;
-    }
-
-    window.location.href = `https://api.whatsapp.com/send?phone=55${phone}&text=${window.encodeURIComponent(text)}`;
+  window.location.href = `https://api.whatsapp.com/send?phone=55${phone}&text=${window.encodeURIComponent(
+    text
+  )}`;
 }
 
-function formatMoeda(value:any) {
-    return parseFloat(value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+function formatMoeda(value: any) {
+  return parseFloat(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
