797e7d3617b5d56bb2d5bb9fe23daed31fa331ff
Murilo Lopes <mloopes11@gmail.com>
2021-05-21 00:09:27 -0300

    change font size header

--- cardapio-frontend/src/components/Header/styles.ts
+++ cardapio-frontend/src/components/Header/styles.ts
@@ -1,41 +1,39 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-    position:fixed;
-    width: 100%;
-    margin: 0 auto;
-    padding: .5rem 1rem;
-    display: flex;
-    flex-direction: row;
-    background-color: #7C1519;
-    margin-bottom: 15px;
-    color: white;
-    z-index: 15;
+  position: fixed;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0.5rem 1rem;
+  display: flex;
+  flex-wrap: row;
+  flex-direction: row;
+  background-color: #7c1519;
+  margin-bottom: 15px;
+  color: white;
+  z-index: 15;
 `;
 
 export const ImgBox = styled.div`
-    width: 33%;
-    display: flex;
-    flex-direction: row;
-    img {
-        width: 30%;
-        margin-left:30%;
-    }
+  width: 33%;
+  display: flex;
+  flex-direction: row;
+  img {
+    width: 30%;
+    margin-left: 30%;
+  }
 `;
 
 export const UserBox = styled.div`
-	margin-left: 20px;
-
-    strong {
-        font-size: 1.5em;
-    }
-    p {
-        margin-top: 10px;
-    }
+  margin-left: 20px;
+  font-size: 1.2rem;
+  p {
+    margin-top: 10px;
+  }
 `;
 
 export const BoxHamb = styled.div`
-    z-index: 5;
-    margin-top: 1px;
-    margin-right: 5%;
+  z-index: 5;
+  margin-top: 1px;
+  margin-right: 5%;
 `;
